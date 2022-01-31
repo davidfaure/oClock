70f0d19a7e12ee94d37f9b7010af349b3e2c65f1
David Faure <aswelldavid@gmail.com>
2022-01-31 17:46:43 +0100

    env to heroku

--- server/config.js
+++ server/config.js
@@ -6,12 +6,16 @@ dotenv.config();
 const pool = mysql.createPool({
   connectionLimit: 1000,
   connectTimeout: 60 * 60 * 1000,
-  host: process.env.HOST || 'localhost',
-  user: process.env.USER || 'root',
-  password: process.env.PASSWORD || 'Stopdiren!mp8',
-  database: process.env.DATABASE || 'o_clock',
-  insecureAuth: true
+  host: process.env.HOST, // eu-cdbr-west-02.cleardb.net
+  user: process.env.USER, // b56cc3bafbfef6
+  password: process.env.PASSWORD, // 0a3bae1f
+  database: process.env.DATABASE, // heroku_994c25509218148
+  // insecureAuth: true
 });
+
+ // mysql://b56cc3bafbfef6:0a3bae1f@eu-cdbr-west-02.cleardb.net/heroku_994c25509218148?reconnect=true
+
+
 
 module.exports = {
   query: function () {
